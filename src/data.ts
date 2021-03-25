7392119a05703b89306d5e1ade76391caba4387a
vmik7 <vmik7@yandex.ru>
2021-03-25 17:50:07 +0300

    Добавил номера слайдов для дебага

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,11 @@
 import { Slide } from './application/types';
 
 export const stories: Slide[] = [
-    { alias: 'slide1', data: { color: 'red', text: 'this is a test' } },
-    { alias: 'slide1', data: { color: 'green' } },
-    { alias: 'slide1', data: { color: 'blue' } },
-    { alias: 'slide2', data: { color: 'white' } },
-    { alias: 'slide2', data: { color: 'yellow' } },
-    { alias: 'slide2', data: { color: 'brown' } },
-    { alias: 'slide2', data: { color: 'coral' } },
+    { alias: 'slide1', data: { color: 'red', text: 'slide1' } },
+    { alias: 'slide1', data: { color: 'green', text: 'slide2' } },
+    { alias: 'slide1', data: { color: 'blue', text: 'slide3' } },
+    { alias: 'slide2', data: { color: 'white', text: 'slide4' } },
+    { alias: 'slide2', data: { color: 'yellow', text: 'slide5' } },
+    { alias: 'slide2', data: { color: 'brown', text: 'slide6' } },
+    { alias: 'slide2', data: { color: 'coral', text: 'slide7' } },
 ];
