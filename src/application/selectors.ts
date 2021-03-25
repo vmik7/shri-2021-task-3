f9249db93bb84d84773184ad373f870209072d6a
vmik7 <vmik7@yandex.ru>
2021-03-25 17:36:09 +0300

    Переключение слайдов по таймеру

--- src/application/selectors.ts
+++ src/application/selectors.ts
@@ -14,8 +14,7 @@ export const createProgressSelector = (state$: Observable<State>) => state$.pipe
 
 export const createCurrentIndexSelector = (state$: Observable<State>) => state$.pipe(
     map(s => s.index),
-    distinctUntilChanged(),
-    mergeMapTo(EMPTY),
+    distinctUntilChanged()
 );
 
 export const createThemeSelector = (state$: Observable<State>) => state$.pipe(
