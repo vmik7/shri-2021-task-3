8f498dd79e4945d4e9028c7ef7a8a71af5fde7f8
vmik7 <vmik7@yandex.ru>
2021-03-26 12:38:47 +0300

    Правильное сравнение объектов distinctUntilChanged

--- src/application/selectors.ts
+++ src/application/selectors.ts
@@ -1,5 +1,5 @@
-import { EMPTY, Observable } from 'rxjs';
-import { distinctUntilChanged, map, mergeMapTo } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 import { DELAY, Slide, State } from './types';
 
 interface SlideData<T> {
@@ -9,7 +9,7 @@ interface SlideData<T> {
 
 export const createProgressSelector = (state$: Observable<State>) => state$.pipe(
     map(({ index, progress }): SlideData<number> => ({ index, value: progress / DELAY })),
-    distinctUntilChanged(),    
+    distinctUntilChanged((a, b) => a.index === b.index && a.value === b.value)
 );
 
 export const createCurrentIndexSelector = (state$: Observable<State>) => state$.pipe(
@@ -19,12 +19,10 @@ export const createCurrentIndexSelector = (state$: Observable<State>) => state$.
 
 export const createThemeSelector = (state$: Observable<State>) => state$.pipe(
     map(s => s.theme),
-    distinctUntilChanged(),
+    distinctUntilChanged()
 );
 
 export const createCurrentDataSelector = (state$: Observable<State>) => state$.pipe(
     map(({ index, stories }): SlideData<Slide> => ({ index, value: stories[index] })),
-    distinctUntilChanged(
-        (a, b) => a.index === b.index && a.value === b.value
-    )
+    distinctUntilChanged((a, b) => a.index === b.index && a.value === b.value)
 );
